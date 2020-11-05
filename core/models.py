6a8bf803dbcc03fdadb8eda8eb61cc249fc15093
hector <hec.jara@alumnos.duoc.cl>
2020-11-05 15:00:36 -0300

    se vuelve a agregar estado poliza

--- core/models.py
+++ core/models.py
@@ -82,9 +82,9 @@ class ASEGURADORA(models.Model):
     id_pais = models.ForeignKey(PAIS, on_delete=models.CASCADE)
 
 
-# class ESTADO_POLIZA(models.Model):
-#     id_estado = models.AutoField(primary_key=True)
-#     nombre_estado = models.CharField(max_length=30, null=False, blank=False)
+class ESTADO_POLIZA(models.Model):
+    id_estado = models.AutoField(primary_key=True)
+    nombre_estado = models.CharField(max_length=30, null=False, blank=False)
 
 
 class SEGURO(models.Model):
@@ -93,7 +93,7 @@ class SEGURO(models.Model):
     fecha_actualizacion = models.DateField(null=False, blank=False)
     id_persona = models.ForeignKey(PERSONA, on_delete=models.CASCADE, null=True, blank=True)
     id_aseguradora = models.ForeignKey(ASEGURADORA, on_delete=models.CASCADE)
-    # id_estado = models.ForeignKey(ESTADO_POLIZA, on_delete=models.CASCADE)
+    id_estado = models.ForeignKey(ESTADO_POLIZA, on_delete=models.CASCADE)
 
 
 class FORMA_PAGO(models.Model):
